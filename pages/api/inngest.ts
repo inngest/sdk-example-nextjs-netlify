c8cde1a530bf76d4ab1432a29a8a1876cd7d6029
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-11-29 12:12:47 +0000

    Use new Inngest function export method for integration testing

--- pages/api/inngest.ts
+++ pages/api/inngest.ts
@@ -1,24 +1,4 @@
-import { Inngest } from "inngest";
 import { serve } from "inngest/next";
-import { Events } from "../../__generated__/inngest";
+import fns from "../../inngest";
 
-const inngest = new Inngest<Events>({ name: "My App" });
-
-const foo = inngest.createFunction(
-  "Hello---my _=👍 :) heere we gooo!",
-  "demo/event.sent",
-  ({ event, ctx, steps }) => ({
-    message: "I ran foo!",
-    data: event.data,
-  })
-);
-
-const bar = inngest.createFunction("Bar", "demo/event.sent", ({ event }) => ({
-  message: "I ran bar!",
-  data: event.data,
-}));
-
-export default serve(inngest, process.env.INNGEST_SIGNING_KEY as string, [
-  foo,
-  bar,
-]);
+export default serve("My Next.js Netlify App", fns);
